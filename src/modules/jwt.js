d6e5565ef38b68b9f53530cdb15916958229d718
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-10-22 19:54:23 +0500

    add jwt

---
+++ src/modules/jwt.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken");
+
+module.exports.createToken = function (user) {
+	return jwt.sign(user, process.env.JWT_SECRET, {
+		expiresIn: "1d",
+	});
+};
+
+module.exports.verifyToken = function (token) {
+	return jwt.verify(token, process.env.JWT_SECRET);
+};
